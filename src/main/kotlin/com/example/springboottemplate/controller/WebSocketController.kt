d24e3f999d1cd5988527814dc14d036b2e6becf3
artemenko_pa <artemenko_pa@magnit.ru>
2021-11-17 21:25:06 +0300

    Add websocket example

--- src/main/kotlin/com/example/springboottemplate/controller/WebSocketController.kt
+++ src/main/kotlin/com/example/springboottemplate/controller/WebSocketController.kt
@@ -2,6 +2,7 @@ package com.example.springboottemplate.controller
 
 import com.example.springboottemplate.dto.request.GreetingRequest
 import com.example.springboottemplate.dto.response.GreetingResponse
+import org.springframework.messaging.handler.annotation.Header
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.messaging.handler.annotation.SendTo
 import org.springframework.stereotype.Controller
@@ -11,7 +12,7 @@ class WebSocketController {
 
     @MessageMapping("/chat")
     @SendTo("/topic/greetings")
-    fun sendMessage(request: GreetingRequest) : GreetingResponse{
-        return GreetingResponse("Hello, ${request.name}")
+    fun sendMessage(@Header(name = "simpSessionId") sessionId: String, request: GreetingRequest) : GreetingResponse{
+        return GreetingResponse("Hello, ${request.name}, your sessionId is $sessionId")
     }
 }
