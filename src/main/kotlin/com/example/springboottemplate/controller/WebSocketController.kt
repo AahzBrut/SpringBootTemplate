8147b0e176b8353e99ce91910b548d7b06281bc7
artemenko_pa <artemenko_pa@magnit.ru>
2021-11-18 15:37:36 +0300

    Refactor websocket controller example

--- src/main/kotlin/com/example/springboottemplate/controller/WebSocketController.kt
+++ src/main/kotlin/com/example/springboottemplate/controller/WebSocketController.kt
@@ -2,37 +2,44 @@ package com.example.springboottemplate.controller
 
 import com.example.springboottemplate.dto.request.GreetingRequest
 import com.example.springboottemplate.dto.response.GreetingResponse
-import org.springframework.messaging.MessageHeaders
+import org.springframework.context.event.EventListener
 import org.springframework.messaging.handler.annotation.Header
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.messaging.handler.annotation.SendTo
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor
-import org.springframework.messaging.simp.SimpMessageType
 import org.springframework.messaging.simp.SimpMessagingTemplate
+import org.springframework.messaging.simp.user.SimpUserRegistry
 import org.springframework.stereotype.Controller
+import org.springframework.web.socket.messaging.SessionConnectedEvent
+import org.springframework.web.socket.messaging.SessionDisconnectEvent
 
 @Controller
 class WebSocketController(
-    val messageTemplate: SimpMessagingTemplate
+    val messageTemplate: SimpMessagingTemplate,
+    val userRegistry: SimpUserRegistry
 ) {
 
     @MessageMapping("/chat")
     @SendTo("/topic/greetings")
     fun sendMessage(@Header(name = "simpSessionId") sessionId: String, request: GreetingRequest): GreetingResponse {
 
+        println("Users: ${userRegistry.users}")
+
         messageTemplate.convertAndSendToUser(
             sessionId,
             "/queue/notifications",
-            GreetingResponse(message = "Message '${request.name}' sent"),
-            createHeaders(sessionId)
+            GreetingResponse(message = "Message '${request.name}' sent")
         )
         return GreetingResponse("Hello, ${request.name}, your sessionId is $sessionId")
     }
 
-    fun createHeaders(sessionId: String): MessageHeaders {
-        return SimpMessageHeaderAccessor.create(SimpMessageType.MESSAGE).also {
-            it.sessionId = sessionId
-            it.setLeaveMutable(true)
-        }.messageHeaders
+    @EventListener(SessionConnectedEvent::class)
+    fun handleSessionConnectedEvent(event: SessionConnectedEvent) {
+        val sessionId = event.message.headers["simpSessionId"].toString()
+        println("Session connected: $sessionId")
+    }
+
+    @EventListener(SessionDisconnectEvent::class)
+    fun handleSessionDisconnectEvent(event: SessionDisconnectEvent) {
+        println("Session disconnected: ${event.message.headers["simpSessionId"]}")
     }
 }
